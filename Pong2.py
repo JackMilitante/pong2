94d037997407f64664951fb086e4ace3e2a52536
Jack Militante <jackmilitante10@gmail.com>
2019-11-01 18:43:13 -0500

    change onkey

--- Pong2.py
+++ Pong2.py
@@ -6,10 +6,13 @@ import os
 
 wn = turtle.Screen()
 wn.title("Pong")
-wn.bgcolor("black")
+wn.bgcolor("grey")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+
+
+#main game loop
 # Score
 score_a = 0
 score_b = 0
@@ -18,7 +21,7 @@ score_b = 0
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("yellow")
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -27,7 +30,7 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("green")
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -36,7 +39,7 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("white")
+ball.color("red")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
@@ -75,10 +78,10 @@ def paddle_b_down():
 
 # Keyboard bindings
 wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
+wn.onkey(paddle_a_up, "w")
+wn.onkey(paddle_a_down, "s")
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down, "Down")
 
 # Main game loop
 while True:
